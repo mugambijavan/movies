15ce9c225911b70a64b47697280b56af5ae826a3
Javoh <117713938+mugambijavan@users.noreply.github.com>
2025-01-29 01:37:26 +0300

    Update index.html title and favicon; add search and star SVG icons; enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,18 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div 
+        className="pattern"
+      />
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' />
+          <h1> <span className='text-gradient'>Finding Movies</span></h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
 
